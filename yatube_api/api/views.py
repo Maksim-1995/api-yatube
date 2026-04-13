2b007fe5d48097d77b46c327c00bcdd6916441b3
Maksim Kosaretskiy <ma.ko1995@mail.ru>
2026-04-13 16:17:37 +0300

    "Update views.pyё"

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -39,8 +39,12 @@ class PostViewSet(viewsets.ModelViewSet):
             serializer = CommentSerializer(data=request.data)
             if serializer.is_valid():
                 serializer.save(author=request.user, post=post)
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    serializer.data, status=status.HTTP_201_CREATED
+                )
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
 
     @action(
         detail=True,
@@ -64,7 +68,9 @@ class PostViewSet(viewsets.ModelViewSet):
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
         elif request.method == 'DELETE':
             comment.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
